3994b2cd7b8eeed0985d56f0cdf9453243e080b7
fkoen001 <fkoen001@PC12-113.intern.mi.hs-rm.de>
2014-06-18 14:08:38 +0200

    HashTree kann aus byte[] erstellt werden. Noch nicht bugfrei

--- src/HashTree.java
+++ src/HashTree.java
@@ -1,22 +1,95 @@
+import java.util.Arrays;
+
+import de.medieninf.ads.ADSTool;
 
 public class HashTree {
-	
+
 	private abstract class Node {
 		public byte[] hash; // 20 byte hash
 	}
-	
+
 	private class NodeInner extends Node {
 		public Node left, right; // Unterknoten
 	}
-	
+
 	private class NodeLeaf extends Node {
 		public byte[] data; // Daten
 	}
-	
+
+	private static byte[] nullHash = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
 	private Node root;
-	
-	public HashTree() {
-		root = new NodeInner();
+
+	public HashTree(byte[] data, int blocksize) {
+
+		int blocks = (int) Math.ceil((double) data.length / blocksize);
+		NodeLeaf[] nodes = new NodeLeaf[blocks];
+
+		for (int i = 0; i < blocks; i++) {
+			byte[] d = new byte[blocksize];
+			System.arraycopy(data, i * blocksize, d, 0, blocksize);
+			NodeLeaf n = new NodeLeaf();
+			n.data = d;
+			n.hash = ADSTool.sha1(d);
+			nodes[i] = n;
+		}
+
+		this.root = getRootNode(nodes);
+
 	}
-	
+
+	private Node getRootNode(Node[] nodes) {
+		return compactNodes(nodes)[0];
+	}
+
+	private Node[] compactNodes(Node[] nodes) {
+
+		// Abbruchbedingung
+		if (nodes.length <= 1) return nodes;
+
+		Node[] newNodes = new Node[(int) Math.ceil(nodes.length / 2d)];
+		for (int i = 0; i < newNodes.length; i++) {
+			NodeInner node = new NodeInner();
+			int index2 = 2 * i + 1;
+			node.left = nodes[2 * i];
+			node.right = (index2 >= nodes.length) ? null : nodes[2 * i + 1];
+			byte[] concat = new byte[node.left.hash.length * 2];
+			System.arraycopy(node.left.hash, 0, concat, 0, node.left.hash.length);
+			System.arraycopy((index2 >= nodes.length) ? nullHash : node.right.hash, 0, concat, node.left.hash.length, node.left.hash.length);
+			node.hash = ADSTool.sha1(concat);
+			newNodes[i] = node;
+		}
+
+		return compactNodes(newNodes);
+
+	}
+
+	public void display() {
+		display(root);
+	}
+
+	public void display(Node node) {
+		if (node instanceof NodeLeaf) {
+			NodeLeaf nodeLeaf = (NodeLeaf) node;
+			System.out.print(bytesToHex(nodeLeaf.hash) + ": ");
+			System.out.println(Arrays.toString(nodeLeaf.data));
+		} else {
+			NodeInner nodeInner = (NodeInner) node;
+			System.out.println(bytesToHex(nodeInner.hash) + ": inner node");
+			if (nodeInner.left != null) display(nodeInner.left);
+			if (nodeInner.right != null) display(nodeInner.right);
+		}
+	}
+
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+	public static String bytesToHex(byte[] bytes) {
+		char[] hexChars = new char[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
+	}
+
 }
--- src/moin.java
+++ src/moin.java
@@ -1,8 +1,23 @@
+import java.util.Arrays;
+import java.util.Random;
 
 public class moin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Random random = new Random();
+		byte[] data = new byte[150];
+		for (int i = 0; i < data.length; i++) {
+			data[i] = (byte) random.nextInt();
+		}
+
+		System.out.println("Generated data:");
+		System.out.println(Arrays.toString(data));
+
+		HashTree ht = new HashTree(data, 30);
+
+		System.out.println("HashTreeData: ");
+		ht.display();
 
 	}
 
