a968d4d89a252c121b7c4036097d90d71cb84c81
fkoen001 <fkoen001@PC12-113.intern.mi.hs-rm.de>
2014-06-18 12:34:19 +0200

    HashTree Node Klassen

--- src/HashTree.java
+++ src/HashTree.java
@@ -1,4 +1,22 @@
 
 public class HashTree {
-
+	
+	private abstract class Node {
+		public byte[] hash; // 20 byte hash
+	}
+	
+	private class NodeInner extends Node {
+		public Node left, right; // Unterknoten
+	}
+	
+	private class NodeLeaf extends Node {
+		public byte[] data; // Daten
+	}
+	
+	private Node root;
+	
+	public HashTree() {
+		root = new NodeInner();
+	}
+	
 }
